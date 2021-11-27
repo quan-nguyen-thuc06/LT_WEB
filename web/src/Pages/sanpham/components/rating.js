5e9ad98841d0912a6af31a36677f5ef14839ee97
quocthanhh <Dangthanh3819@gmail.com>
2021-11-27 20:07:15 +0700

    update san pham chi tiet

---
+++ web/src/Pages/sanpham/components/rating.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+import './rating.css'
+import { FaStar } from 'react-icons/fa'
+
+const Rating = () =>{
+  const [rating,setRating] = useState(null);
+  const [hover,setHover] = useState(null);
+
+  return(
+    <div>
+        {[...Array(5)].map((star,i) => {
+          const ratingValue = i + 1;
+          return (
+            <label>
+              <input type="radio" 
+                name="rating" 
+                value={ratingValue} 
+                onClick={() => setRating(ratingValue)} />
+
+              <FaStar className="star" 
+                color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"} 
+                size={35} 
+                onMouseEnter={() => setHover(ratingValue)} 
+                onMouseLeave={() => setHover(null)} />
+            </label>
+          );
+         })}
+    </div>
+  );
+}
+
+export default Rating
