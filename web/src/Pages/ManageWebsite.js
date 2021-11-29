c3f20389663f5053b30cc759fbcb710910d5e4e1
TienLe <lengoctienbrvt@gmail.com>
2021-11-29 23:02:38 +0700

    admin website

--- web/src/Pages/ManageWebsite.js
+++ web/src/Pages/ManageWebsite.js
@@ -1,7 +1,126 @@
 import './ManageProduct.css'
-import React, { useContext, useEffect, useState }  from 'react'
+import React ,{ useState, Fragment, useEffect } from 'react'
+import axios from 'axios'
+import {Form, Button, Row, Col, InputGroup, Grid } from 'react-bootstrap'
 function ManageWebsite(){
+    const [data,setData]=useState([])
+    useEffect(async () => {
+        await axios.get('http://localhost/Official/LT_WEB/server/api/employment/read.php')
+        .then(response => {
+            setData(response.data)
+            //console.log(response.data)
+        })
+        .catch(error => console.log(error))
+    }, [])
+    
+    if(data.length>0){
+        console.log(data)
+    }  
+    const [idEdit, setIdEdit]= useState(0)
+    function handleEdit(event){
+        //setIdEdit(event.target.name)
+        
+    }
+    function RenderHiring(){
+        return <Fragment >{data.slice(0, data.length).map((index) => {
+            return(
+            <tr>
+                <td>{index.id}</td>
+                <td>{index.area}</td>
+                <td>{index.deadline}</td>
+                <td>{index.address}</td>
+                <td>
+                    <a class="add" title="Add" data-toggle="tooltip">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"/>
+                        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                    </svg>    
+                    </a>
+                    <a class="edit" title="Edit" data-toggle="tooltip" onClick={() => handleEdit()} data-bs-toggle="modal" data-bs-target="#exampleModal0" name={index.id} >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                        </svg>
+                    </a>
+                    <a class="delete" title="Delete" data-toggle="tooltip">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                        </svg>
+                    </a>
+                </td>
+                <div class="modal fade" id="exampleModal0" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Sửa vị trí tuyển dụng</h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                        </div>
 
+                        <div class="modal-body">
+                        <form class="needs-validation" novalidate>
+                            <div class="table-title row">
+                                <div class="col-12">
+                                    <label for="exampleInput1">Vị trí tuyển dụng</label>
+                                    {/* <input type="text" class="form-control" id="exampleInput1" value={index.area} onChange={test} required></input> */}
+                                    <Form.Control
+                                        required
+                                        type="text"
+                                        id="exampleInput1"
+                                        defaultValue={index.area}
+                                        // onChange={test}
+                                    />
+                                </div>
+                                <div class="col-6">
+                                    <label for="exampleInput0">STT</label>
+                                    {/* <input type="number" class="form-control" id="exampleInput0" value={index.id} onChange={test} required></input> */}
+                                    <Form.Control
+                                        required
+                                        type="number"
+                                        id="exampleInput0"
+                                        defaultValue={idEdit}
+                                        // onChange={test}
+                                    />
+                                </div>
+                                <div class="col-6">
+                                    <label for="exampleInput2">Han nộp</label>
+                                    {/* <input type="date" class="form-control" id="exampleInput2" value={index.deadline} onChange={test} required></input> */}
+                                    <Form.Control
+                                        required
+                                        type="date"
+                                        id="exampleInput2"
+                                        defaultValue={index.deadline}
+                                        // onChange={test}
+                                    />
+                                </div>
+                                <div class="col-12">
+                                    <label for="exampleInput3">Địa điểm</label>
+                                    {/* <input type="text" class="form-control" id="exampleInput3" value={index.address} onChange={test} required></input> */}
+                                    <Form.Control
+                                        required
+                                        type="text"
+                                        id="exampleInput3"
+                                        defaultValue={index.address}
+                                        // onChange={test}
+                                    />
+                                </div>
+                            </div>
+                        </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div> 
+                    
+            </tr>
+            )
+            
+        })}
+        {console.log(data)}
+        </Fragment>
+    }
 
   return(
     <div class="container">
@@ -11,21 +130,20 @@ function ManageWebsite(){
                     <div class="row">
                         <div class="col-sm-8"><h2>Tuyển dụng</h2></div>
                         <div class="col-sm-4">
-                            <button type="button" class="btn btn-info add-new" data-bs-toggle="modal" data-bs-target="#addHiring"><i class="fa fa-plus" ></i> Thêm mới</button>
+                        <button type="button" class="btn btn-info add-new" data-bs-toggle="modal" data-bs-target="#exampleModal1"><i class="fa fa-plus" ></i> Thêm mới</button>
                         </div>
                     </div>
 
-                    <div class="modal" id="addHiring">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
+                    <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Thêm vị trí tuyển dụng</h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                        </div>
 
-                            <div class="modal-header">
-                                <h4 class="modal-title">Thêm vị trí tuyển dụng</h4>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                            </div>
-
-                            <div class="modal-body">
-                            <form>
+                        <div class="modal-body">
+                        <form>
                                 <div class="table-title row">
                                     <div class="col-12">
                                         <label for="exampleInput3">Vị trí tuyển dụng</label>
@@ -44,17 +162,15 @@ function ManageWebsite(){
                                         <input type="text" class="form-control" id="exampleInput3" required></input>
                                     </div>
                                 </div>
-                            </form> 
-                                
-                            </div>
-
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Đóng</button>
-                            </div>
-
-                            </div>
+                            </form>
                         </div>
-                    </div>   
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>  
                 </div>
                 <table class="table table-bordered">
                     <thead>
@@ -67,7 +183,8 @@ function ManageWebsite(){
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        <RenderHiring />
+                        {/* <tr>
                             <td>1</td>
                             <td>Kế toán</td>
                             <td>13/1/2022</td>
@@ -141,7 +258,7 @@ function ManageWebsite(){
                                     </svg>
                                 </a>
                             </td>
-                        </tr>      
+                        </tr>       */}
                     </tbody>
                 </table>
             </div>
